32833aae690bb2ca7eab59db37b41d9dc8b94909
abdullahalharun <harun750@gmail.com>
2021-02-01 21:47:33 +0600

    update

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -36,11 +36,11 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
 		<div class="col2-set input-group" id="customer_details">
-			<div class="col-6">
+			<div class="col-lg-6 col-md-6 col-12">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 			</div>
 
-			<div class="col-6">
+			<div class="col-lg-6 col-md-6 col-12">
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
 		</div>
