b21bf276836c578603f85b88f63a96dcc70387d0
abdullahalharun <harun750@gmail.com>
2021-10-18 16:42:12 +0600

    update single

--- single.php
+++ single.php
@@ -150,16 +150,17 @@ if ( have_posts() ) {
                     <div class="sticky">
                         <!--Section: Dynamic Content Wrapper-->
                         <section>
-                          <div id="dynamic-content"></div>
+                          <div id="dynamic-content">                       
 
+                            <!--Sidebar-->
+                                <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+                                <?php dynamic_sidebar( 'sidebar' ); ?>
+                                <?php endif; ?>
+                            <!--/.Sidebar-->
+
+                            </div>
                         </section>
                         <!--Section: Dynamic Content Wrapper-->
-
-                        <!--Sidebar-->
-                            <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-                            <?php dynamic_sidebar( 'sidebar' ); ?>
-                            <?php endif; ?>
-                        <!--/.Sidebar-->
 
                     </div>
                     <!-- Sticky content -->
